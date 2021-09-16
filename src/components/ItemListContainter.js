e194390fb1bdd20ca27e99c76af08de6e8f89bd5
eaglecontrol <sistemas@aguilacontrol.com>
2021-09-16 18:51:32 -0500

    Se agrega el useState en ItemListContainer.js para una futura implementaciÃon

--- src/components/ItemListContainter.js
+++ src/components/ItemListContainter.js
@@ -1,12 +1,14 @@
 /* Entregable 03 */
-import React from 'react'
+import React, {useState} from 'react'
 
 function ItemListContainter(props) {
 
-    const itemCount = props.count > 0  ? (<span class="badge bg-primary cart-item-count">{props.count}</span>) : "";
+    const [itemCount, setitemCount] = useState(props.count)
+
+    const itemCountTag = itemCount > 0  ? (<span class="badge bg-primary cart-item-count">{props.count}</span>) : "";
 
     return (
-        itemCount
+        itemCountTag
     )
 }
 
