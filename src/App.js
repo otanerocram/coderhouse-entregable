a0aa765ba4255252ef6f37d9a3cce94b8a1d9dc2
eaglecontrol <sistemas@aguilacontrol.com>
2021-09-15 20:58:21 -0500

    modificacion

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Tienda de Renato Beltran <code>src/App.js</code>.
+          Entregable <code>src/App.js</code>.
         </p>
       </header>
     </div>
